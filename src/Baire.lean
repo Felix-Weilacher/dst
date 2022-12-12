dc07844c3120cf1694a7ccb68fdc452bddf38600
Felix-Weilacher <fweilach@andrew.cmu.edu>
2022-12-11 23:22:40 -0500

    more KU

--- src/Baire.lean
+++ src/Baire.lean
@@ -1,6 +1,7 @@
 import topology.metric_space.baire
 import measure_theory.constructions.borel_space
 import order.filter.curry
+import topology.homeomorph
 
 #check @residual
 
@@ -74,6 +75,22 @@ begin
   apply subset_Union₂,
 end
 
+lemma eventually.eventually_eq {f : filter α} {p q : α → Prop} 
+  (hp : ∀ᶠ x in f, p x) (heq : q =ᶠ[f] p) : ∀ᶠ x in f, q x :=
+begin
+  rw [filter.eventually,← eventually_eq_univ] at *,
+  exact heq.trans hp,
+end
+
+lemma frequently.eventually_eq {f : filter α} {p q : α → Prop} 
+  (hp : ∃ᶠ x in f, p x) (heq : q =ᶠ[f] p) : ∃ᶠ x in f, q x :=
+begin
+  rw [filter.frequently] at *,
+  intros h, 
+  apply hp,
+  exact h.eventually_eq heq.compl.symm,
+end
+
 end filter
 end cfilter
 
@@ -119,6 +136,33 @@ begin
   rw [is_comeager,comeager,mem_countable_generate_iff],
   refl,
 end
+
+lemma is_meager_iff_sUnion {s : set α} : is_meager s ↔ 
+  ∃ T : set (set α), (∀ t ∈ T, is_closed t ∧ interior t = ∅) ∧ T.countable ∧ s ⊆ ⋃₀ T :=
+begin
+  rw [is_meager,is_comeager_iff_sInter],
+  sorry,
+end
+
+lemma is_comeager_of_sInter {S : set (set α)} (Sct : S.countable) 
+  (hS : ∀ s ∈ S, is_comeager s) : is_comeager ⋂₀ S :=
+(countable_sInter_mem Sct).mpr hS
+
+lemma is_meager_of_sUnion {S : set (set α)} (Sct : S.countable) 
+  (hS : ∀ s ∈ S, is_meager s) : is_meager ⋃₀ S :=
+begin
+  rw[is_meager, compl_sUnion],
+  apply is_comeager_of_sInter (Sct.image _),
+  rintros s ⟨t,tS,rfl⟩,
+  apply hS,
+  exact tS,
+end
+
+lemma is_comeager.eventually_eq {s t : set α} (hs : is_comeager s) (hst : t =ᵇ s) : 
+  is_comeager t := eventually.eventually_eq hs hst
+
+lemma is_meager.eventually_eq {s t : set α} (hs : is_meager s) (hst : t =ᵇ s) : 
+  is_meager t := is_comeager.eventually_eq hs hst.compl
 
 @[simp]
 theorem is_meager_compl_iff {s : set α} : is_meager sᶜ ↔ is_comeager s := 
@@ -220,6 +264,21 @@ end
 end baire_measurable_set
 
 
+section homeomorphism
+
+#check homeomorph
+variables {β : Type*} [topological_space β] (f : α ≃ₜ β)
+
+#check filter.map f
+
+theorem map_comeager_eq_comeager : filter.map f (comeager α) = comeager β :=
+begin
+  sorry,
+end
+
+
+end homeomorphism
+
 section KU 
 
 open topological_space function
@@ -240,6 +299,10 @@ begin
   exact hS,
 end
 
+lemma filter.frequently_curry_iff {α β: Type*} {f : filter α} {g : filter β} {p : α × β → Prop} :
+  (∃ᶠ z in f.curry g, p z) ↔ (∃ᶠ x in f, ∃ᶠ y in g, p (x,y)) := 
+by simp only[filter.frequently, eventually_curry_iff, not_not]
+
 lemma is_open.section (h : is_open A) (x : α) : is_open (A.curry x) :=
 begin
   refine continuous_def.mp _ A h,
@@ -251,7 +314,7 @@ end
 #check λ y, λ x, A.curry x y
 
 --set_option pp.all true
-lemma curry_le_comeager [second_countable_topology β] : 
+theorem curry_le_comeager [second_countable_topology β] : 
   (comeager α).curry (comeager β) ≤ comeager (α × β) :=
 begin
   intros A hA,
@@ -287,15 +350,67 @@ begin
   exact ⟨y,yu,xyt⟩,
 end
 
-lemma eventually_eventually_of_comeager [second_countable_topology β] (hA : is_comeager A) : 
+lemma topological_space.is_topological_basis.is_open_Union_countable_basis 
+  [second_countable_topology α] {b : set (set α)} (hb : is_topological_basis b) 
+  {U : set α} (hU : is_open U) : ∃ t : set (set α), t.countable ∧ t ⊆ b ∧ ⋃₀ t = U :=
+begin
+  rw hb.open_iff_eq_sUnion at hU,
+  rcases hU with ⟨S,hS,rfl⟩,
+  apply exists_imp_exists _ (is_open_sUnion_countable S _), { tauto },
+  refine hS.trans _,
+  intro u, 
+  exact hb.is_open,
+end
+
+lemma is_meager.prod_left {u : set α} {v : set β} (hu : is_meager u) : is_meager (u ×ˢ v) :=
+begin
+  rw [is_meager, is_comeager_iff_sInter] at *,
+  rcases hu with ⟨T, hT, Tct, Tu⟩,
+  use (λ t : set α, t ×ˢ univ) '' T,
+  refine ⟨_, by apply Tct.image, _⟩,
+  { rintros s ⟨t,tT,rfl⟩,
+    specialize hT t tT,
+    split, { exact hT.1.prod is_open_univ, },
+    exact hT.2.prod dense_univ,},
+  intros p,
+  simp only [sInter_image, mem_Inter, mem_prod, mem_univ, and_true, mem_compl_iff, not_and],
+  intros pT pu,
+  exfalso,
+  exact Tu pT pu,
+end
+
+lemma is_meager.prod_right {u : set α} {v : set β} (hv : is_meager v) : is_meager (u ×ˢ v) :=
+begin
+  rw [is_meager, ← is_comeager_def, ← map_comeager_eq_comeager (homeomorph.prod_comm _ _), 
+    homeomorph.coe_prod_comm, mem_map, preimage_compl, preimage_swap_prod, 
+    is_comeager_def, is_comeager_compl_iff],
+  apply hv.prod_left,
+end
+
+theorem eventually_eventually_of_comeager [second_countable_topology β] (hA : is_comeager A) : 
   ∀ᶠ x in (comeager α), ∀ᶠ y in (comeager β), (x,y) ∈ A := curry_le_comeager hA
 
-lemma frequently_frequently_of_nonmeager [second_countable_topology α]      
+theorem frequently_frequently_of_not_meager [second_countable_topology α]      
   [second_countable_topology β] 
   (hmeas : baire_measurable_set A) (hA : ¬ is_meager A) : 
   ∃ᶠ x in (comeager α), ∃ᶠ y in (comeager β), (x,y) ∈ A :=
 begin
-  sorry,
+  rcases hmeas with ⟨U,Uop,hU⟩,
+  rcases ((@is_topological_basis_opens α _).prod 
+    (@is_topological_basis_opens β _)).is_open_Union_countable_basis Uop with ⟨T, Tct, Trect, rfl⟩,
+  obtain ⟨t, tT, ht⟩ : ∃ t, t ∈ T ∧ ¬ is_meager t,
+  { contrapose! hA,
+    apply is_meager.eventually_eq _ hU,
+    exact is_meager_of_sUnion Tct hA, },
+  rcases Trect tT with ⟨u, v, -, -, rfl⟩, 
+  have hu : ¬ is_meager u := λ h, ht h.prod_left,
+  have hv : ¬ is_meager v := λ h, ht h.prod_right,
+  apply (@filter.frequently_curry_iff _ _ _ _ A).mp,
+  apply frequently.eventually_eq _ (curry_le_comeager hU),
+  apply frequently.mono _ (subset_sUnion_of_mem tT),
+  simp only [filter.frequently_curry_iff, mem_prod, 
+    frequently_and_distrib_left, frequently_and_distrib_right],
+  exact ⟨hu, hv⟩,
 end
 
 theorem baire_measurable_sections_left [second_countable_topology β] 
@@ -322,9 +437,58 @@ begin
   contrapose, 
   intro hA,
   simp only [not_eventually, ← mem_compl_iff],
-  apply frequently_frequently_of_nonmeager hmeas.compl,
+  apply frequently_frequently_of_not_meager hmeas.compl,
   simp[hA],
 end
+
+/-
+theorem curry_eq_comeager [second_countable_topology α] [second_countable_topology β] 
+  (hmeas : baire_measurable_set A) : 
+  is_comeager A ↔ ∀ᶠ x in (comeager α), ∀ᶠ y in (comeager β), (x,y) ∈ A :=
+begin
+  split, { apply curry_le_comeager },
+  intro hA,
+  rcases hmeas.compl with ⟨U,Uop,hU⟩,
+  rcases ((@is_topological_basis_opens α _).prod 
+    (@is_topological_basis_opens β _)).is_open_Union_countable_basis Uop with ⟨T, Tct, Trect, rfl⟩,
+  have : ∀ t ∈ T, is_meager t,
+  { intros t ht,
+    rcases Trect ht with ⟨u, v, uop, vop, rfl⟩, 
+    suffices : (is_meager u) ∨ (is_meager v),
+    { cases this with hu hv, 
+      { exact hu.prod_left, },
+      exact hv.prod_right, },
+    by_contra',
+    let f := (comeager α).curry (comeager β),
+    have h1 : A ∈ f := hA,
+    have h2 : (⋃₀ T)ᶜ ∈ f,
+    { rw ← eventually_eq_univ at *,
+      calc
+      (⋃₀ T)ᶜ =ᶠ[f] Aᶜᶜ : curry_le_comeager (hU.compl.symm)
+      ... = A : by rw compl_compl
+      ... =ᶠ[f] univ : h1 },
+    have h3 : (u ×ˢ v)ᶜ ∈ f,
+    { apply filter.mem_of_superset h2,
+      rw [compl_subset_compl],
+      exact subset_sUnion_of_mem ht,  },
+    have h4 : ∃ᶠ x in (comeager α), ∃ᶠ y in (comeager β), (x,y) ∈ (u ×ˢ v),
+    { simp only [prod_mk_mem_set_prod_eq, frequently_and_distrib_left, frequently_and_distrib_right], 
+      exact this, },
+    have h5 : ∃ᶠ p in f, p ∈ (u ×ˢ v),
+    { rw[filter.frequently, eventually_curry_iff],
+      simp_rw [not_eventually,not_not],
+      exact h4, },
+    contradiction, },
+  rw [is_comeager_iff_eventually_eq_univ],
+  calc
+  A = Aᶜᶜ : by rw compl_compl
+  ... =ᵇ (⋃₀ T)ᶜ : hU.compl
+  ... =ᵇ univ : by 
+  { rw [compl_sUnion, eventually_eq_univ, countable_sInter_mem],
+    { rintros - ⟨t,tT,rfl⟩, apply this, exact tT, },
+    apply Tct.image, },
+end
+-/
 
 end KU
 
